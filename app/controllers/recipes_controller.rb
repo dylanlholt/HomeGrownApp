93bc0473e932a2dda281bedc100d8578b39dd517
Dylan Holt <dylan.lee.holt@gmail.com>
2015-11-23 04:26:24 +0000

    Added New and Edit views; Created Messages partial

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -8,4 +8,40 @@ class RecipesController < ApplicationController
     @recipe = Recipe.find(params[:id])
   end
   
+  def new
+    @recipe = Recipe.new
+  end
+  
+  def edit
+    @recipe = Recipe.find(params[:id])
+  end
+  
+  def create
+    @recipe = Recipe.new(recipe_params)
+    @recipe.chef = Chef.find(1)
+    if @recipe.save
+      flash[:success] = "Recipe successfully created!"
+      redirect_to recipe_path(@recipe)
+    else
+      render 'new'
+    end
+  end
+  
+  def update
+    @recipe = Recipe.find(params[:id])
+    
+    if @recipe.update(recipe_params)
+      flash[:success] = "Recipe successfully updated!"
+      redirect_to recipe_path(@recipe)
+    else
+      render 'edit'
+    end
+  end
+  
+  private
+    
+    def recipe_params
+      params.require(:recipe).permit(:name, :summary, :description)
+    end
+    
 end
